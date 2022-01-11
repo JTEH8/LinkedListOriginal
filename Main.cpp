cbe4f3a38c59135d9c1969b1f659275efdce9c7c
JTEH8 <jeffteh22@gmail.com>
2022-01-10 19:14:05 -0800

    Added More to Main

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Node.h"
+
+using namespace std;
+void add(Node* currentNode,int newValue);
+void print(Node* currentNode, Node* next);
+
+int main(){
+Node* head = new Node();
+
+}
+
+void add(Node* currentNode,int newValue){
+    current
+}
