b91d90431636a8f79be78cfa886da4c440a13c1f
Jeffrey Teh <59184460+Jeffderp22@users.noreply.github.com>
2022-01-10 20:07:21 -0800

    Updated to Video

--- Main.cpp
+++ Main.cpp
@@ -6,10 +6,37 @@ void add(Node* currentNode,int newValue);
 void print(Node* currentNode, Node* next);
 
 int main(){
+Node* temp = new Node();
 Node* head = new Node();
-
+add(head,1);
+add(temp,1);
+print(temp, head);
+print(head, temp);
 }
 
 void add(Node* currentNode,int newValue){
-    current
+    Node* current = currentNode;
+    if(current == NULL){
+    currentNode = new Node();
+    currentNode->setValue(newValue);
+    }
+    else{
+        while(currentNode->getNext() != NULL){
+            current = currentNode->getNext();
+        }
+        current->setNext(new Node());
+        current->getNext()->setValue(newValue);
+    }
+}
+
+void print (Node* currentNode,Node* next){
+    if(currentNode == next){
+        cout << "Student List: " ;
+    }
+    if(next != NULL){
+        cout << next->getValue() << " ";
+        print(next->getNext(),next->getNext());
+    }else{
+    //go running
+} 
 }
